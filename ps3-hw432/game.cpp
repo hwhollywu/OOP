65725bc0e66c3be56fcb896e9fb968f31f65fa1b
hwhollywu <hwhollywu@gmail.com>
2018-10-08 17:16:24 -0400

    use switch-return instead of ifs

--- ps3-hw432/game.cpp
+++ ps3-hw432/game.cpp
@@ -17,49 +17,48 @@ play(){
 
 	cout << "Welcome to the Think-a-Dot Game!" << endl;
 	cout << "Type 'H' for user instructions." << endl; 
-	while (1) {
+	for (;;) {
 		cout << "Please enter your command: ";
 		cin >> ch;
 		ch = toupper(ch);
 
-		// case 1: simulate the action of the machine
-		if (ch == 'A' || ch == 'B' || ch == 'C'){
+		switch(ch){
+			// case 1: simulate the action of the machine
+			case 'A':
+			case 'B':
+			case 'C':
+				tad.play(ch);
+				continue;
+			// case 2: reset all points to be left or right 
+			case 'L':
+			case 'R':
+				tad.reset(ch);
+				continue;
+			// case 3: prints the state of the machine
+			case 'P':
+				tad.print(cout);
+				continue;
+			// case 4: prints a brief version of these instructions
+			case 'H':
+				cout << "- User Instruction for Think-a-Dot Game -" << endl;
+				cout << "Valid inputs include the following single letters: " << endl;
+				cout << "A/B/C: to drop the ball in respective hole; " << endl;
+				cout << "L/R: to reset all points to the left / right; " << endl;
+				cout << "P: to print the current state of the machine;" << endl;
+				cout << "Q: to quit the game;" << endl;
+				continue;
+			// case 5: exits the program.
+			case 'Q': 
+				cout << "Program exit.";
+				return;
+			default: 
+			// Ask user for new command, if the input is invalid 
+				cin.clear();
+				cout << "The input is not valid! Type 'H' for help." << endl;
+		}
 
-			tad.play(ch);
-			continue;
-		}
-		// case 2: reset all points to be left or right 
-		if (ch == 'L' || ch == 'R'){
-			tad.reset(ch);
-			continue;
-		}
-		// case 3: prints the state of the machine
-		if (ch == 'P'){
-			tad.print(cout);
-			continue;
-		}
-		// case 4: prints a brief version of these instructions
-		if (ch == 'H'){
-			cout << "- User Instruction for Think-a-Dot Game -" << endl;
-			cout << "Valid inputs include the following single letters: " << endl;
-			cout << "A/B/C: to drop the ball in respective hole; " << endl;
-			cout << "L/R: to reset all points to the left / right; " << endl;
-			cout << "P: to print the current state of the machine;" << endl;
-			cout << "Q: to quit the game;" << endl;
-			continue;
-		}
-		// case 5: exits the program.
-		if (ch == 'Q'){
-			break;
-		} 
-
-		// Ask user for new command, if the input is invalid 
-		else{
-			cin.clear();
-			cout << "The input is not valid! Type 'H' for help." << endl;
-		}
 	}
 
-	cout << "Program exit.";
+
 
 }
